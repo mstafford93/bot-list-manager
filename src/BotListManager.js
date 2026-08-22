83012c2701685d5b7f057f2c55650f6c36303773
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-22 16:13:35 +0100

    add colorChange depending on status, and css

--- src/BotListManager.js
+++ src/BotListManager.js
@@ -13,12 +13,30 @@ const BotListManager = () => {
     
   };
 
+  const colorChange = (status) => {
+    console.log(status);
+    if (status === "Completed") {
+      return ({
+                backgroundColor: "rgb(12, 250, 63)",
+            })
+    } else if (status === "Stopped") {
+      return ({
+                backgroundColor: "rgb(255, 21, 21)",
+            })
+    } else {
+      return ({
+        backgroundColor: "rgb(91, 21, 255)",
+        color: "white"
+      })
+    }
+  }
+
   /*Change Object to prop*/
   return (
     <div className="bot-list-manager">
       <h1>Bot List Manager</h1>
       <ul>
-        {bots.map((bot) => <li key={bot.id}>{bot.id}-{bot.name}-{bot.status} /*Change Object to prop*/
+        {bots.map((bot) => <li key={bot.id} style={colorChange(bot.status)}>{bot.id}-{bot.name}-{bot.status}
           <button onClick={() => triggerJob(bot.id)}>Trigger Job</button></li>)}
       </ul>
     </div>
